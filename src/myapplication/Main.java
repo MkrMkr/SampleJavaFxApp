7f43b11424b5f2978fefdceb98ec764aeab03458
Mirosław Król <mkrol@concisesoftware.com>
2016-10-31 15:13:30 +0100

    Responsive without bootstrapfx - part I

--- src/myapplication/Main.java
+++ src/myapplication/Main.java
@@ -1,10 +1,16 @@
 package myapplication;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import myapplication.controllers.BaseController;
+import myapplication.controllers.PhoneNumberController;
 
 public class Main extends Application {
 
@@ -12,7 +18,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         StageFactory factory = StageFactory.INSTANCE;
-        factory.registerStage(primaryStage);
+        factory.setCurrentStage(primaryStage);
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource(AllScenes
                 .PhoneNumberScreen
--- src/myapplication/controllers/PhoneNumberController.java
+++ src/myapplication/controllers/PhoneNumberController.java
@@ -1,10 +1,43 @@
 package myapplication.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import myapplication.AllScenes;
+import myapplication.StageFactory;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PhoneNumberController extends BaseController {
+public class PhoneNumberController extends BaseController implements Initializable {
+
+    @FXML
+    public Button continueButton;
+    @FXML
+    public AnchorPane anchorPane;
+    @FXML
+    public ScrollPane scrollPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        StageFactory stageFactory = StageFactory.INSTANCE;
+        Stage currentStage = stageFactory.getCurrentStage();
+
+        anchorPane.prefWidthProperty().bind(currentStage.widthProperty());
+        anchorPane.prefHeightProperty().bind(currentStage.heightProperty());
+        scrollPane.prefWidthProperty().bind(currentStage.widthProperty());
+        scrollPane.prefHeightProperty().bind(currentStage.heightProperty());
+    }
+
+    public PhoneNumberController() {
+        StageFactory factory = StageFactory.INSTANCE;
+        Stage primaryStage = factory.getCurrentStage();
+    }
 
     public void continueButtonClick() {
         try {
@@ -13,6 +46,7 @@ public class PhoneNumberController extends BaseController {
             e.printStackTrace();
         }
     }
+
 
     private void goToAccessCodeScene() throws IOException {
         switchSceneTo(AllScenes.AccessCodeScreen.getAccessCodeScene().getTitle(),
