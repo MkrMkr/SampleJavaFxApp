8a2a69a296f7e518c3308817b2767408d3b2e849
Mirosław Król <mkrol@concisesoftware.com>
2016-11-10 09:30:39 +0100

    Profile screen responsive horizontally

--- src/main/java/ProfileController.java
+++ src/main/java/ProfileController.java
@@ -3,9 +3,11 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
@@ -33,6 +35,8 @@ public class ProfileController extends BaseController implements Initializable {
     @FXML
     public TextField enterYourNameField;
     @FXML
+    public CheckBox receiveNotificationsCheckbox;
+    @FXML
     private ImageView profileIcon;
 
     @Override
@@ -43,29 +47,26 @@ public class ProfileController extends BaseController implements Initializable {
         toolbar.widthProperty().bind(currentStage.widthProperty());
         profileBackground.fitWidthProperty().bind(currentStage.widthProperty());
 
+        Circle circle = new Circle(45);
+        circle.setCenterX(46);
+        circle.setCenterY(45);
+        profileIcon.setClip(circle);
 
-//        Circle circle = new Circle(45);
-//        circle.setCenterX(46);
-//        circle.setCenterY(45);
-//        profileIcon.setClip(circle);
+        double addPhotoTitleOffset = 10;
+        double halfOfProfileIconWidth = profileIcon.getFitWidth() / 2;
 
-     //   profileIcon.setX(200);
-      //  profileIcon.xProperty().bind(anchorPane.widthProperty());
+        profileIcon.layoutXProperty().bind(anchorPane.widthProperty().divide(2).subtract(halfOfProfileIconWidth));
+        addPhotoTitle.layoutXProperty().bind(anchorPane.widthProperty()
+                .divide(2)
+                .subtract(halfOfProfileIconWidth)
+                .add(addPhotoTitleOffset));
 
-        anchorPane.widthProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-
-            }
-        });
-
-        System.out.println("java version: "+System.getProperty("java.version"));
+        System.out.println("java version: " + System.getProperty("java.version"));
         System.out.println("javafx.version: " + System.getProperty("javafx.version"));
-
+        Pane pane = new Pane();
     }
 
     public void useMyFacebookDetails(ActionEvent actionEvent) {
-
     }
 
     public void continueButtonClick(ActionEvent actionEvent) {
